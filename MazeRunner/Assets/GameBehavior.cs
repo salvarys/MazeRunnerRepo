e8df919e0b6d1a1d533f377c8f48f25096c598a5
salvarys <123325469+salvarys@users.noreply.github.com>
2023-11-15 11:33:21 -0700

    Updated the UI for the game

--- MazeRunner/Assets/GameBehavior.cs
+++ MazeRunner/Assets/GameBehavior.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string labelText = "Collect all to win!!!";
+    public int maxItems = 3;
+    public bool showWinScreen = false;
+
+    private int _itemsCollected = 0;
+
+    public int Items
     {
-        
+        get { return _itemsCollected; }
+
+        set
+        {
+            _itemsCollected = value;
+            Debug.LogFormat("Items: {0}", _itemsCollected);
+
+            if (_itemsCollected >= maxItems)
+            {
+                labelText = "You've found all the items!";
+                showWinScreen = true;
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                labelText = "Item found, only" + (maxItems -
+                    _itemsCollected) + " more to go!";
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnGUI()
     {
-        
+
+        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " +
+            _itemsCollected);
+
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height -
+            50, 300, 50), labelText);
+
+        if (showWinScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100,
+               Screen.height / 2 - 50, 200, 100), "YOU WON!"))
+            {
+                SceneManager.LoadScene(0);
+                Time.timeScale = 1.0f;
+            }
+        }
     }
 }
